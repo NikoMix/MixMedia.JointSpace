0e05b800f00571373c87bc884bceb7d3cf511bbd
Niko Mix <mix.niko@outlook.com>
2017-10-17 00:12:18 +0200

    Fixed return Types for Async void

--- MixMedia.JointSpace/Manager/AudioManager.cs
+++ MixMedia.JointSpace/Manager/AudioManager.cs
@@ -19,12 +19,12 @@ namespace MixMedia.JointSpace.Manager
             return await _client.GetAsync<Volume>($"/1/audio/volume");
         }
 
-        public async void SetAudioVolume(int volume)
+        public async Task SetAudioVolume(int volume)
         {
             await _client.PostAsync($"/1/audio/volume", new SetVolumeRequest{ Volume = volume});
         }
 
-        public async void SetAudioMute(bool muted)
+        public async Task SetAudioMute(bool muted)
         {
             await _client.PostAsync($"/1/audio/volume", new SetVolumeRequest { Muted = muted });
         }
@@ -34,7 +34,7 @@ namespace MixMedia.JointSpace.Manager
         /// </summary>
         /// <param name="muted"></param>
         /// <param name="volume"></param>
-        public async void SetAudioMuteAndVolumne(bool muted, int volume)
+        public async Task SetAudioMuteAndVolumne(bool muted, int volume)
         {
             await _client.PostAsync($"/1/audio/volume", new SetVolumeRequest { Muted = muted, Volume = volume});
         }
